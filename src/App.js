5b9be7c34cf713b1c3645f97b257f19e33f519eb
Diana Guadalupe García Aguirre <69890392+DianaA96@users.noreply.github.com>
2023-04-24 13:31:52 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import LandingPage from './views/LandingPage';
-import HeaderDiseno from './components/HeaderDiseno';
-import HeaderSencillo from './components/HeaderSencillo';
 
 function App() {
   return (
     <div className="App">
-      
-      <HeaderDiseno>
-
-      </HeaderDiseno>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+
