90fe27f4e0ab8e7bd64b65be22d88fe29ebcc3c9
jose heron samperio leon <joseheronsamperio@gmail.com>
2023-04-24 16:09:04 -0600

    crea componente tabla laboratorio y su diseno, al igual que la vista de reporte laboratorio

---
+++ src/components/tablaLaboratorio.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Tabla = ({ tableData, copyTableData }) => {
+    return (
+        <>
+            <div className="cabeza">
+                <span className="turno">Primer Turno</span>
+                <button className="custom-button" onClick={copyTableData}>
+                    <div className="button-content">
+                        <span class="material-symbols-outlined"> content_copy</span>
+                        <span className="button-text">Copiar valores</span>
+                    </div>
+                </button>
+            </div>
+
+            <table className="tablona">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th style={{ color: "#EF7B30" }}>
+                            Ag
+                            <span>g/ton</span>
+                        </th>
+                        <th>
+                            Pb
+                            <span>%</span>
+                        </th>
+                        <th style={{ color: "#EF7B30" }}>
+                            Zn
+                            <span>%</span>
+                        </th>
+                        <th>
+                            Cu
+                            <span>%</span>
+                        </th>
+                        <th style={{ color: "#EF7B30" }}>
+                            Fe
+                            <span>%</span>
+                        </th>
+                        <th>
+                            Sb
+                            <span>%</span>
+                        </th>
+                        <th style={{ color: "#EF7B30" }}>
+                            As
+                            <span>%</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map(row => (
+                        <tr key={row.id}>
+                            <td>{row.col0}</td>
+                            <td>{row.col1}</td>
+                            <td>{row.col2}</td>
+                            <td>{row.col3}</td>
+                            <td>{row.col4}</td>
+                            <td>{row.col5}</td>
+                            <td>{row.col6}</td>
+                            <td>{row.col7}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <hr />
+        </>
+    )
+}
+
+export default Tabla;
