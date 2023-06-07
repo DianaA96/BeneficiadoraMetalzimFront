ea0bb12792b8ac77d8124f9c7c553e4df3432db1
Carlos Manuel Gonzalez Vallejo <A01276000@tec.mx>
2023-06-06 23:57:15 -0600

    "Conexión Historial"

--- src/views/HistorialBascula.js
+++ src/views/HistorialBascula.js
@@ -7,69 +7,57 @@ import GraficasLine from "../components/GraficasLine";
 import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 function HistorialBascula({ rol }) {
-  const reportes = [
-    {
-      id: "RE01",
-      fecha: "26/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-    {
-      id: "RE02",
-      fecha: "27/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-    {
-      id: "RE03",
-      fecha: "28/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-    {
-      id: "RE04",
-      fecha: "29/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-    {
-      id: "RE05",
-      fecha: "12/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-    {
-      id: "RE06",
-      fecha: "15/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-    {
-      id: "RE07",
-      fecha: "21/04/2023",
-      existenciaInicial: 412,
-      acarreo: 562,
-      trituradas: 343,
-      patios: 623,
-    },
-  ];
+  const [dataAcarreo, setDataAcarreo] = useState([]);
+  const [historial, setHistorial] = useState([]);
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+
+
+
+  var check = 0;
+
+  useEffect(() => {
+    setStatus("loading");
+    axios
+      .get(`http://localhost:3050/gerente/grapHistoricas`)
+      .then((result) => {
+        setDataAcarreo(result.data);
+        setStatus("resolved");
+      })
+      // Aquí van las demás solicitudes
+      .catch((error) => {
+        setError(error);
+        setStatus("error");
+      });
+  }, [check]);
+
+  
+  useEffect(() => {
+    setStatus("loading");
+    axios
+      .get(`http://localhost:3050/gerente/movMineralTable`)
+      .then((result) => {
+        setHistorial(result.data);
+        setStatus("resolved");
+      })
+      // Aquí van las demás solicitudes
+      .catch((error) => {
+        setError(error);
+        setStatus("error");
+      });
+  }, []);
+
+  console.log(historial)
+
+
+
+
+
   const columns = React.useMemo(() => [
     { field: "id", headerName: "ID", flex: 1, maxWidth: 80 },
     { field: "fecha", headerName: "Fecha", flex: 1, maxWidth: 120 },
     {
-      field: "existenciaInicial",
+      field: "inicial",
       headerName: "Existencia Inicial",
       flex: 1,
       minWidth: 100,
@@ -94,30 +82,18 @@ function HistorialBascula({ rol }) {
       ],
     },
   ]);
-  const [dataAcarreo, setDataAcarreo] = useState([]);
-  const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
-  var check = 0;
-  useEffect(() => {
-    setStatus("loading");
-    axios
-      .get(`http://localhost:3050/gerente/grapHistoricas`)
-      .then((result) => {
-        setDataAcarreo(result.data);
-        setStatus("resolved");
-      })
-      // Aquí van las demás solicitudes
-      .catch((error) => {
-        setError(error);
-        setStatus("error");
-      });
-  }, [check]);
+
+
+
+
   if (status === "error") {
     return (
       // NO RESPONDE EL BACK 404
       <Navigate to="/Error404" replace={true} />
     );
   }
+
+
   if (status == "resolved") {
    /* console.log("dataAcarreo", dataAcarreo);
     console.log("dataAcarreo", dataAcarreo.acarreo[0].totalAcarreo);
@@ -152,7 +128,7 @@ function HistorialBascula({ rol }) {
           />
           <ListaReportes
             columns={columns}
-            data={reportes}
+            data={historial}
             titulo="Todos los reportes"
           ></ListaReportes>
           <div
