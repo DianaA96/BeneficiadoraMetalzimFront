da4334e8dfbf9320ae2e0a73886b0620e721ef1c
Verónica Martínez Morales <veronicamartinezmorales@MacBook-Air-de-Veronica.local>
2023-06-07 18:02:32 -0600

    Conexion graficas Historial General completo

--- src/views/HistorialGerencia.js
+++ src/views/HistorialGerencia.js
@@ -114,18 +114,21 @@ function HistorialGerencia({ rol }) {
   ]);
 
   const [dataGraficas, setDataGraficas] = useState([]);
+  const [precioValor, setPrecioValor] = useState([]);
   const [status, setStatus] = useState("idle");
   const [error, setError] = useState(null);
   var check = 0;
   useEffect(() => {
     setStatus("loading");
+    axios.get(`http://localhost:3050/gerente/grapHistoricas`).then((result) => {
+      setDataGraficas(result.data);
+    });
     axios
-      .get(`http://localhost:3050/gerente/grapHistoricas`)
+      .get(`http://localhost:3050/gerente/grapLiquidacion`)
       .then((result) => {
-        setDataGraficas(result.data);
+        setPrecioValor(result.data);
         setStatus("resolved");
       })
-      // Aquí van las demás solicitudes
       .catch((error) => {
         setError(error);
         setStatus("error");
@@ -139,14 +142,24 @@ function HistorialGerencia({ rol }) {
   }
   if (status == "resolved") {
     console.log("dataGraficas", dataGraficas);
+    console.log("precioValor", precioValor);
 
     // Variable para saber la cantidad de registros(meses) que tiene concentrados
     let myConcentrado = Object.keys(dataGraficas.concentrados);
+    let myPrecio = Object.keys(precioValor.metales);
+    let myValor = Object.keys(precioValor.valor);
+
+    // Variable para calcular la suma total por mes de Valor de mineral
+    var totalValor = 0;
+
     // Arreglo para conocer la información de cada mes en el Objeto concentrados de la consulta
     var mes = [];
 
     // Arreglo para formatear la información de la consulta de concentrados
     var arrayConcentrado = [];
+    var arrayPrecio = [];
+    var arrayValor = [];
+
     for (var i = 0; i < myConcentrado.length; i++) {
       // Convierte a número el mes de los registros de la consulta
       mes = Math.floor(myConcentrado[i]);
@@ -204,9 +217,125 @@ function HistorialGerencia({ rol }) {
         valorConcentrado.push(arrConc[x]);
       }
       arrayConcentrado.push(valorConcentrado);
-      arrConc = []; // Limpiando la variable totalTritur para la siguiente iteración
+      arrConc = []; // Limpiando la variable arrConc para la siguiente iteración
     }
     console.log("arrayConcentrado", arrayConcentrado);
+
+    for (var i = 0; i < myPrecio.length; i++) {
+      // Convierte a número el mes de los registros de la consulta
+      mes = Math.floor(myPrecio[i]);
+
+      // Arreglo que para agregar los valores uno por uno para formatear la información
+      var valorPrecio = [];
+
+      // Variable que guarda los registros por mes que regresa la consulta. Es un arreglo
+      var arrPecio = precioValor.metales[mes];
+
+      // Casos para agregar el mes que viene en la consulta valorPrecio
+      // Los números de los casos corresponden a cada mes, del 1 al 12
+      switch (mes) {
+        case 1:
+          valorPrecio.push("Enero");
+          break;
+        case 2:
+          valorPrecio.push("Febrero");
+          break;
+        case 3:
+          valorPrecio.push("Marzo");
+          break;
+        case 4:
+          valorPrecio.push("Abril");
+          break;
+        case 5:
+          valorPrecio.push("Mayo");
+          break;
+        case 6:
+          valorPrecio.push("Junio");
+          break;
+        case 7:
+          valorPrecio.push("Julio");
+          break;
+        case 8:
+          valorPrecio.push("Agosto");
+          break;
+        case 9:
+          valorPrecio.push("Septiembre");
+          break;
+        case 10:
+          valorPrecio.push("Octubre");
+          break;
+        case 11:
+          valorPrecio.push("Noviembre");
+          break;
+        case 12:
+          valorPrecio.push("Diciembre");
+          break;
+        default:
+          console.log(`Lo siento, ${mes} no corresponde a ningún mes.`);
+      }
+      // For para iterar los datos de arrPecio y poder agregar uno por uno al arreglo final arrayPrecio
+      for (var x = 0; x < arrPecio.length; x++) {
+        valorPrecio.push(arrPecio[x]);
+      }
+      arrayPrecio.push(valorPrecio);
+      arrPecio = []; // Limpiando la variable arrPecio para la siguiente iteración
+    }
+    console.log("arrayPrecio", arrayPrecio);
+
+    // For para iterar los registros que vienen en la consulta del objeto Valor
+    for (var i = 0; i < myValor.length; i++) {
+      // Convierte a número el mes de los registros de la consulta
+      mes = Math.floor(myValor[i]);
+      // Función que hace la suma de las cantidades de los registros por mes
+      // Regresa un número entero, correspondiente a la cantidad de valor total por cada mes
+      precioValor.valor[mes].forEach(function (a) {
+        totalValor += a;
+      });
+      // Casos para agregar un nuevo arreglo en el fromato ["mes", cantidad] al arreglo final arrayAcarreo
+      // Los números de los casos corresponden a cada mes, del 1 al 12
+      switch (mes) {
+        case 1:
+          arrayValor.push(["Enero", totalValor]);
+          break;
+        case 2:
+          arrayValor.push(["Febrero", totalValor]);
+          break;
+        case 3:
+          arrayValor.push(["Marzo", totalValor]);
+          break;
+        case 4:
+          arrayValor.push(["Abril", totalValor]);
+          break;
+        case 5:
+          arrayValor.push(["Mayo", totalValor]);
+          break;
+        case 6:
+          arrayValor.push(["Junio", totalValor]);
+          break;
+        case 7:
+          arrayValor.push(["Julio", totalValor]);
+          break;
+        case 8:
+          arrayValor.push(["Agosto", totalValor]);
+          break;
+        case 9:
+          arrayValor.push(["Septiembre", totalValor]);
+          break;
+        case 10:
+          arrayValor.push(["Octubre", totalValor]);
+          break;
+        case 11:
+          arrayValor.push(["Noviembre", totalValor]);
+          break;
+        case 12:
+          arrayValor.push(["Diciembre", totalValor]);
+          break;
+        default:
+          console.log(`Lo siento, ${mes} no corresponde a ningún mes.`);
+      }
+      totalValor = 0; // Limpiando la variable totalValor para la siguiente iteración
+    }
+    console.log("arrayValor", arrayValor);
 
     return (
       <>
@@ -250,15 +379,16 @@ function HistorialGerencia({ rol }) {
             <GraficasLine
               titulo="Precios de los metales"
               descripcion="(USD)"
-              data={arrayConcentrado}
-              long={arrayConcentrado.length}
+              data={arrayPrecio}
+              long={arrayPrecio.length}
             ></GraficasLine>
-            {/*
+
             <GraficasArea
-              titulo="Valor del mineral (USD)"
-              class="contGraficas"
+              titulo={"Valor del mineral (USD)"}
+              descripcion={"en toneladas"}
+              data={arrayValor}
+              long={arrayValor.length}
             ></GraficasArea>
-            */}
           </div>
         </body>
         <footer>
